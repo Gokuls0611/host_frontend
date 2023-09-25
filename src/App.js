7475547ed50436744187232ff568fb6867d5aba7
Gokulfast <gokulperan1234@gmail.com>
2023-09-25 09:14:52 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Register from './components/Register';
 import Login from './components/Login';
 import {Routes,Route} from 'react-router-dom';
@@ -6,10 +7,11 @@ import Home from './components/Home';
 import ForgotPassword from './components/ForgotPassword';
 import About from './components/About';
 import Service from './components/Service';
+import  Cart  from './components/Cart';
 import Pagenotfound from './components/Pagenotfound';
 import Contact from './components/Contact';
 import './App.css';
-
+import './components/style.css'
 function App() {
   return (
     <div className="App">
@@ -17,13 +19,14 @@ function App() {
       <Routes>
       <Route path='/' element={<Home/>}></Route>
       <Route path='/login' element={<Login/>}></Route>
-      <Route path='/register' element={<Register/>}></Route>
+      <Route path='/register' element= {<Register/>}></Route>
       <Route path='/ForgotPassword' element={<ForgotPassword/>}></Route>
       <Route path='/about' element = {<About/>}></Route>
       <Route path='/services' element = {<Service/>}></Route>
       <Route path='/contact' element = {<Contact/>}></Route>
       <Route path="*" element = {<Pagenotfound/>}></Route>
-      </Routes> 
+      <Route path='/cart'element={<Cart/>}></Route>
+      </Routes>
     </div>
   );
 }
