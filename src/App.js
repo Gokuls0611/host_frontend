d1f8eb3b37c62c4281cd4242db94976f0b31eeae
Gokulfast <gokulperan1234@gmail.com>
2023-10-07 22:31:43 +0530

    app error

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import Register from './components/Register';
-import Login from './components/Login';
 import {Routes,Route} from 'react-router-dom';
 import Menu from './components/Menu';
 import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
 import ForgotPassword from './components/ForgotPassword';
-import About from './components/About';;
-import  Cart  from './components/Cart';
+import About from './components/About';
 import Placeorder from './components/Placeorder';
-import Pagenotfound from './components/Pagenotfound';
+import  Cart  from './components/Cart';
 import Contact from './components/Contact';
+import Pagenotfound from './components/Pagenotfound';
 import './App.css';
 import './components/style.css'
+
+
 function App() {
   return (
     <div className="App">
@@ -23,9 +25,9 @@ function App() {
       <Route path='/ForgotPassword' element={<ForgotPassword/>}></Route>
       <Route path='/about' element = {<About/>}></Route>
       <Route path='/orders' element = {<Placeorder/>}></Route>
+      <Route path='/cart'element={<Cart/>}></Route>
       <Route path='/contact' element = {<Contact/>}></Route>
       <Route path="*" element = {<Pagenotfound/>}></Route>
-      <Route path='/cart'element={<Cart/>}></Route>
       </Routes>
     </div>
   );
