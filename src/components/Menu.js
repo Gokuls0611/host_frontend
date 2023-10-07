92af0a09ea36a7f547b43ff28b2931d97132281b
Gokulfast <gokulperan1234@gmail.com>
2023-10-07 22:34:03 +0530

    add order link

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -35,6 +35,7 @@ export default function Menu() {
         <div className="navbar">
         <ul className="navbar-links">
             <li><NavLink to='/' className="navbar-link">Home</NavLink></li>
+            <li><NavLink to='/orders' className="navbar-link">Orders</NavLink></li>
             <li><NavLink to ='/about' className="navbar-link">About</NavLink></li>
             <li><NavLink to='/contact' className="navbar-link">Contact</NavLink></li>
         </ul>
