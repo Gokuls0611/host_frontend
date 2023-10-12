9603aa264e4d4cd551a38e046fc2ac875b0d8546
Gokulfast <gokulperan1234@gmail.com>
2023-10-12 12:19:25 +0530

    link change

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import axios from 'axios'
 import {Navbar} from './Navbar'
 import { message } from 'antd'
