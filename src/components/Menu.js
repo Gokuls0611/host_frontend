6f44c3bd122648d5bf1ac92b8be69a7345e5a9f1
Gokul S <92523815+Gokuls0611@users.noreply.github.com>
2023-10-25 07:27:01 +0530

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -38,7 +38,6 @@ export default function Menu() {
               <li><NavLink to='/' className="navbar-link">Home</NavLink></li>
               <li><NavLink to ='/about' className="navbar-link">About</NavLink></li>
               <li><NavLink to='/orders' className="navbar-link">Orders</NavLink></li>
-              <li><NavLink to='/contact' className="navbar-link">Contact</NavLink></li>
           </ul>
           {
             auth?
